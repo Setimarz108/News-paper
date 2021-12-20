61c6e800f966b9b1be2f0e6b175f952cc669d311
Carlos Gozalbes <carlosfgmendoza@gmail.com>
2021-12-20 13:02:03 +0100

    dasf

--- news/src/App.js
+++ news/src/App.js
@@ -1,31 +1,22 @@
-<<<<<<< HEAD
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
 import Jumbotron from './components/Jumbotron';
 import Footer from './components/Footer';
-=======
-import logo from "./logo.svg";
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Navbar from "./components/Navbar";
-import FeaturedPost from "./components/Featured-post";
-import SinglePost from "./components/SinglePost";
->>>>>>> a9e1a89927f2f818b203a6bb5ec9246662b81624
+import FeaturedPost from './components/Featured-post';
+import SinglePost from './components/SinglePost';
 
 function App() {
   return (
     <div className="App mx-auto">
-<<<<<<< HEAD
      <Navbar/>
      <Jumbotron/>
-     <Footer/>
-=======
-      <Navbar />
+     
+      
       <FeaturedPost />
       <SinglePost />
->>>>>>> a9e1a89927f2f818b203a6bb5ec9246662b81624
+      <Footer/>
     </div>
   );
 }
