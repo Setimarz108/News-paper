54b9abfb2871ac0385b38430778dc9edaa060de0
stefantanasa <liketanasa@gmail.com>
2021-12-20 13:45:01 +0000

    Update FetchData.jsx

--- news/src/components/FetchData.jsx
+++ news/src/components/FetchData.jsx
@@ -4,15 +4,17 @@ import React, { Component } from 'react';
 class FetchData extends React.Component {
     state = {
         parameter: "q",
-        query: "lodnon"
-
-
+        query: "london",
+        articles: []
     }
     componentDidMount = async ()=>{
         try{
             let response = await fetch (`https://newsapi.org/v2/top-headlines?${this.state.parameter}=${this.state.query}&apiKey=1931895ad8f647eabfcc4f8434c25d34`)
             if (response.ok){
                 console.log("Is ok")
+                let data = await response.json()
+                this.setState({articles: data})
+                
             }
         }catch{
             console.log("Something went wrong")
