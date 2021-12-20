a9e1a89927f2f818b203a6bb5ec9246662b81624
stefantanasa <liketanasa@gmail.com>
2021-12-20 11:58:32 +0000

    update

--- news/src/App.js
+++ news/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
 import FeaturedPost from "./components/Featured-post";
+import SinglePost from "./components/SinglePost";
 
 function App() {
   return (
